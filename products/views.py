cf4e90dd825e762119138b87b87c885b385d7c54
Kakha Khinikadze <KhinikadzeK@quantumart.ru>
2022-11-27 22:13:21 +0300

    -test-context

--- products/views.py
+++ products/views.py
@@ -8,3 +8,21 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'Store',
+        'header': 'Welcome',
+        'username': 'David',
+        'products': [
+            {'name': 'Houdey', 'price': 6000.00},
+            {'name': 'Jacket', 'price': 5000.00},
+            {'name': 'Top', 'price': 1020.00},
+        ],
+        'promotion': True,
+        'products_of_promotion':[
+            {'name': 'Frog', 'price': 500},
+        ]
+    }
+    return render(request, 'products/test-context.html', context)
